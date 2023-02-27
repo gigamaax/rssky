c20eca4b4f60f086fd1259c604bbef8420cb4a8c
max <mmmatsumoto@ucdavis.edu>
2023-02-27 08:58:43 -0800

    refactor(lualine): update lualine colors

--- lua/lualine/themes/risky.lua
+++ lua/lualine/themes/risky.lua
@@ -1,40 +1,42 @@
 local palette = require("risky.palette")
 
-local risky = {}
+return {
+  normal = {
+    a = { bg = palette.blue_900, fg = palette.white },
+    b = { bg = palette.blue_600, fg = palette.white },
+    c = { bg = palette.gray_800, fg = palette.blue_300 },
+  },
 
-risky.normal = {
-  a = { bg = palette.blue_900, fg = palette.white },
-  b = { bg = palette.blue_700, fg = palette.white },
-  c = { bg = palette.gray_800, fg = palette.blue_300 },
+  insert = {
+    a = { bg = palette.red_900, fg = palette.white },
+    b = { bg = palette.red_600, fg = palette.white },
+    c = { bg = palette.gray_800, fg = palette.red_300 },
+  },
+
+  visual = {
+    a = { bg = palette.purple_900, fg = palette.white },
+    b = { bg = palette.purple_600, fg = palette.white },
+    c = { bg = palette.gray_800, fg = palette.purple_300 },
+  },
+
+  -- inverse of insert
+  replace = {
+    a = { bg = palette.gray_800, fg = palette.red_300 },
+    b = { bg = palette.red_600, fg = palette.white },
+    c = { bg = palette.red_900, fg = palette.white },
+  },
+
+  -- inverse of visual
+  command = {
+    a = { bg = palette.gray_800, fg = palette.purple_300 },
+    b = { bg = palette.purple_600, fg = palette.white },
+    c = { bg = palette.purple_900, fg = palette.white },
+  },
+
+  -- variant of normal
+  inactive = {
+    a = { bg = palette.gray_900, fg = palette.blue_300 },
+    b = { bg = palette.gray_900, fg = palette.blue_300 },
+    c = { bg = palette.gray_900, fg = palette.blue_300 },
+  },
 }
-
-risky.insert = {
-  a = { bg = palette.purple_500, fg = palette.white },
-  b = { bg = palette.purple_300, fg = palette.white },
-  c = { bg = palette.gray_800, fg = palette.purple_100 },
-}
-
-risky.command = {
-  a = { bg = palette.blue_900, fg = palette.white },
-  b = { bg = palette.blue_700, fg = palette.white },
-}
-
-risky.visual = {
-  a = { bg = palette.green_500, fg = palette.white },
-  b = { bg = palette.green_300, fg = palette.white },
-  c = { bg = palette.gray_800, fg = palette.green_100 },
-}
-
-risky.replace = {
-  a = { bg = palette.gray_800, fg = palette.blue_300 },
-  b = { bg = palette.blue_700, fg = palette.white },
-  c = { bg = palette.blue_900, fg = palette.white },
-}
-
-risky.inactive = {
-  a = { bg = palette.gray_800, fg = palette.red_100 },
-  b = { bg = palette.gray_800, fg = palette.red_100 },
-  c = { bg = palette.gray_800, fg = palette.red_100 },
-}
-
-return risky
